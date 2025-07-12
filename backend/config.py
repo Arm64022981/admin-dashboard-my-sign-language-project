8d713672d6c209f2f00fcfbd7978aa782561a569
parin khamthep <137470001+Arm64022981@users.noreply.github.com>
2025-07-12 20:26:57 +0700

    update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 DB_CONFIG = {
     'dbname': 'signlang',
     'user': 'admin',
-    'password': 'urXHjHHIJ4DRFVDUfnoaIgmWyBTOxFP3',  
+    'password': 'urXHjHHIJ4DRFVDUfnoaIgmWyBTOxFP3',
+    'host': 'dpg-d1p5srbuibrs73ddosf0-a.render.com',
     'port': 5432
 }
