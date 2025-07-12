8e6a2df3d64037f1e4c03a02508d2c9ebeff743d
parin khamthep <137470001+Arm64022981@users.noreply.github.com>
2025-07-12 20:30:17 +0700

    update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,6 @@ DB_CONFIG = {
     'dbname': 'signlang',
     'user': 'admin',
     'password': 'urXHjHHIJ4DRFVDUfnoaIgmWyBTOxFP3',
-    'host': 'dpg-d1p5srbuibrs73ddosf0-a.render.com',
+    'host': 'dpg-d1p5srbuibrs73ddosf0-a.oregon-postgres.render.com',
     'port': 5432
 }
